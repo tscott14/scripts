c61cd0a713e21dad470214d2efe04bd59a8a37e9
Tristan Scott <tristanascott123@gmail.com>
2022-08-16 22:26:39 -0500

    Added Work in Progress Prolouge Script

---
+++ daily-prolouge.sh
@@ -0,0 +1,489 @@
+#! /bin/bash
+
+function get_sep_prolouge() {
+	case $1 in
+		'01') echo 'https://youtu.be/ufz5uASVB2A' && return;;
+		'02') echo 'https://youtu.be/Xa1Lszhg40E' && return;;
+		'03') echo 'https://youtu.be/FB2hTkWUt2o' && return;;
+		'04') echo 'https://youtu.be/LeQmJboTssM' && return;;
+		'05') echo 'https://youtu.be/9FgJXrLpsQ0' && return;;
+		'06') echo 'https://youtu.be/1Jw0bnMA5As' && return;;
+		'07') echo 'https://youtu.be/7OKt9YZuEjY' && return;;
+		'08') echo 'https://youtu.be/QIFwJK5_MoE' && return;;
+		'09') echo 'https://youtu.be/K7yU2YWqJhM' && return;;
+		'10') echo 'https://youtu.be/FX8GAjN2gzA' && return;;
+		'11') echo 'https://youtu.be/8GNVkgJF9l4' && return;;
+		'12') echo 'https://youtu.be/T912L7Ik76k' && return;;
+		'13') echo 'https://youtu.be/tO2L7XfC_iw' && return;;
+		'14') echo 'https://youtu.be/Ia-kqtfvs5g' && return;;
+		'15') echo 'https://youtu.be/H3mE7ap4WPk' && return;;
+		'16') echo 'https://youtu.be/qSHdJ_B0GaU' && return;;
+		'17') echo 'https://youtu.be/u8ziaDt84xI' && return;;
+		'18') echo 'https://youtu.be/7VHkXgSO3Qw' && return;;
+		'19') echo 'https://youtu.be/cdSn7AITVgM' && return;;
+		'20') echo 'https://youtu.be/DYqr0IZH52U' && return;;
+		'21') echo 'https://youtu.be/nzEbYpPXXp0' && return;;
+		'22') echo 'https://youtu.be/9Kwb4K9YTIo' && return;;
+		'23') echo 'https://youtu.be/ESSmiaathus' && return;;
+		'24') echo 'https://youtu.be/iccn0RmJTIE' && return;;
+		'25') echo 'https://youtu.be/q5SPT9X_MPg' && return;;
+		'26') echo 'https://youtu.be/7EmFpHrJM9I' && return;;
+		'27') echo 'https://youtu.be/qwnUbiskZqo' && return;;
+		'28') echo 'https://youtu.be/x2zDQU5Kjeg' && return;;
+		'29') echo 'https://youtu.be/EwMXkW2_kJw' && return;;
+		'30') echo 'https://youtu.be/GAV9gN7kaQw' && return;;
+		*) echo "ERROR: invalid day entry: $1" && exit 1;
+	esac
+}
+
+function get_oct_prolouge() {
+	case $1 in
+		'01') echo 'https://youtu.be/UMX41M5iJzQ' && return;;
+		'02') echo 'https://youtu.be/NW7cRlkgm7w' && return;;
+		'03') echo 'https://youtu.be/TN0uaGUNbjc' && return;;
+		'04') echo 'https://youtu.be/2CM-EwUKufY' && return;;
+		'05') echo 'https://youtu.be/uwyn4iPM4No' && return;;
+		'06') echo 'https://youtu.be/MSw7lZxQ0ow' && return;;
+		'07') echo 'https://youtu.be/TjrZC6Z8X8I' && return;;
+		'08') echo 'https://youtu.be/lXFexnDyvdw' && return;;
+		'09') echo 'https://youtu.be/cMu4n-Q5tYw' && return;;
+		'10') echo 'https://youtu.be/ULQIdeDgnSM' && return;;
+		'11') echo 'https://youtu.be/0h47PWtZA8A' && return;;
+		'12') echo 'https://youtu.be/5kjQgokM44s' && return;;
+		'13') echo 'https://youtu.be/5_zlyEGUMJg' && return;;
+		'14') echo 'https://youtu.be/n2Bc0fJ8eKk' && return;;
+		'15') echo 'https://youtu.be/lSNC3dEXasc' && return;;
+		'16') echo 'https://youtu.be/9_QbvNGAV6s' && return;;
+		'17') echo 'https://youtu.be/8XnGKLZuU68' && return;;
+		'18') echo 'https://youtu.be/Pz966FXQlFk' && return;;
+		'19') echo 'https://youtu.be/UrzkVWUcVAs' && return;;
+		'20') echo 'https://youtu.be/yqIpg-CaNs8' && return;;
+		'21') echo 'https://youtu.be/AkTLhm_K3oQ' && return;;
+		'22') echo 'https://youtu.be/E4vsPPfP0xM' && return;;
+		'23') echo 'https://youtu.be/LkiqJBQxuq4' && return;;
+		'24') echo 'https://youtu.be/6M0APziTo9Q' && return;;
+		'25') echo 'https://youtu.be/f_nEzqXrAoU' && return;;
+		'26') echo 'https://youtu.be/wNsV_Y5xU9Q' && return;;
+		'27') echo 'https://youtu.be/if9x8UscsjE' && return;;
+		'28') echo 'https://youtu.be/WFqO7i3yf4s' && return;;
+		'29') echo 'https://youtu.be/Bc8y4ayzVUs' && return;;
+		'30') echo 'https://youtu.be/APgJXgK2cB4' && return;;
+		'31') echo 'https://youtu.be/X4mQILJCfn4' && return;;
+		*) echo "ERROR: invalid day entry: $1" && exit 1;
+	esac
+}
+
+function get_nov_prolouge() {
+	case $1 in
+		'01') echo 'https://youtu.be/mUvpehsBZdo' && return;;
+		'02') echo 'https://youtu.be/xxcySH2P-6E' && return;;
+		'03') echo 'https://youtu.be/wKMUn-8gxFk' && return;;
+		'04') echo 'https://youtu.be/wvZJte9mJr4' && return;;
+		'05') echo 'https://youtu.be/s-NhqRDv9yU' && return;;
+		'06') echo 'https://youtu.be/3fHBYRwMdTM' && return;;
+		'07') echo 'https://youtu.be/PxhAkjlASGw' && return;;
+		'08') echo 'https://youtu.be/CEWHrRZ-nA8' && return;;
+		'09') echo 'https://youtu.be/0gt6mrn9iko' && return;;
+		'10') echo 'https://youtu.be/epUDWWyxwgY' && return;;
+		'11') echo 'https://youtu.be/dJ7rnptYfbw' && return;;
+		'12') echo 'https://youtu.be/KzXP0IFhPEM' && return;;
+		'13') echo 'https://youtu.be/SNMyyJDzN4Y' && return;;
+		'14') echo 'https://youtu.be/wrSeWq2N6ys' && return;;
+		'15') echo 'https://youtu.be/gJpM6XfxIp8' && return;;
+		'16') echo 'https://youtu.be/i6WRd6Nu964' && return;;
+		'17') echo 'https://youtu.be/6z58JquGUBM' && return;;
+		'18') echo 'https://youtu.be/BwbdEiRay-Q' && return;;
+		'19') echo 'https://youtu.be/l25YMN4-m6M' && return;;
+		'20') echo 'https://youtu.be/2uYP4BM_sKs' && return;;
+		'21') echo 'https://youtu.be/yWndlGXvmVo' && return;;
+		'22') echo 'https://youtu.be/gfeCszKT3P0' && return;;
+		'23') echo 'https://youtu.be/Jd3dnUajb4E' && return;;
+		'24') echo 'https://youtu.be/ULgtM8fV_M4' && return;;
+		'25') echo 'https://youtu.be/ArZtnVB_pH8' && return;;
+		'26') echo 'https://youtu.be/BSt8vz616WA' && return;;
+		'27') echo 'https://youtu.be/nzT8N3YxBrM' && return;;
+		'28') echo 'https://youtu.be/MSPgyqcOu7w' && return;;
+		'29') echo 'https://youtu.be/rQM8m2_eLAk' && return;;
+		'30') echo 'https://youtu.be/UopyEUWbtc0' && return;;
+		*) echo "ERROR: invalid day entry: $1" && exit 1;
+	esac
+}
+
+function get_dec_prolouge() {
+	case $1 in
+		'01') echo 'https://youtu.be/6ZIYAfP9jm8' && return;;
+		'02') echo 'https://youtu.be/2TgyE7aWzKM' && return;;
+		'03') echo 'https://youtu.be/j4MLqpt0clA' && return;;
+		'04') echo 'https://youtu.be/qVutDY3gByI' && return;;
+		'05') echo 'https://youtu.be/MeSbQ0aglLs' && return;;
+		'06') echo 'https://youtu.be/b3qGDyE9Cg8' && return;;
+		'07') echo 'https://youtu.be/aSTTSyr7fC0' && return;;
+		'08') echo 'https://youtu.be/_r2dMkvdl30' && return;;
+		'09') echo 'https://youtu.be/Tp-WQD8dYi0' && return;;
+		'10') echo 'https://youtu.be/0wKmjnkI45g' && return;;
+		'11') echo 'https://youtu.be/L9ioAC5v_hc' && return;;
+		'12') echo 'https://youtu.be/w6ZOdWqCd5E' && return;;
+		'13') echo 'https://youtu.be/c2x9CtN6n7E' && return;;
+		'14') echo 'https://youtu.be/pgfq-DCs1s4' && return;;
+		'15') echo 'https://youtu.be/0AbTsQw9eVQ' && return;;
+		'16') echo 'https://youtu.be/wUw8vW8OdTA' && return;;
+		'17') echo 'https://youtu.be/km3qeOVKcU0' && return;;
+		'18') echo 'https://youtu.be/9tkOB40DqWY' && return;;
+		'19') echo 'https://youtu.be/EKHU3RnZrmk' && return;;
+		'20') echo 'https://youtu.be/jwJxsu0GobY' && return;;
+		'21') echo 'https://youtu.be/bk_z3E67ftM' && return;;
+		'22') echo 'https://youtu.be/MmqYnzVKYaE' && return;;
+		'23') echo 'https://youtu.be/NkvEbS--37g' && return;;
+		'24') echo 'https://youtu.be/jR1DoUYQ3wQ' && return;;
+		'25') echo 'https://youtu.be/sx9KOoRV5Ww' && return;;
+		'26') echo 'https://youtu.be/LB4_oSUelco' && return;;
+		'27') echo 'https://youtu.be/XMfGUJPpEJM' && return;;
+		'28') echo 'https://youtu.be/yWRI0e6yQlE' && return;;
+		'29') echo 'https://youtu.be/UMXRHyZl-3A' && return;;
+		'30') echo 'https://youtu.be/o7DaUFHIlVY' && return;;
+		'31') echo 'https://youtu.be/icedtl4apr8' && return;;
+		*) echo "ERROR: invalid day entry: $1" && exit 1;
+	esac
+}
+
+function get_jan_prolouge() {
+	case $1 in
+		'01') echo '' && return;;
+		'02') echo '' && return;;
+		'03') echo '' && return;;
+		'04') echo '' && return;;
+		'05') echo '' && return;;
+		'06') echo '' && return;;
+		'07') echo '' && return;;
+		'08') echo '' && return;;
+		'09') echo '' && return;;
+		'10') echo '' && return;;
+		'11') echo '' && return;;
+		'12') echo '' && return;;
+		'13') echo '' && return;;
+		'14') echo '' && return;;
+		'15') echo '' && return;;
+		'16') echo '' && return;;
+		'17') echo '' && return;;
+		'18') echo '' && return;;
+		'19') echo '' && return;;
+		'20') echo '' && return;;
+		'21') echo '' && return;;
+		'22') echo '' && return;;
+		'23') echo '' && return;;
+		'24') echo '' && return;;
+		'25') echo '' && return;;
+		'26') echo '' && return;;
+		'27') echo '' && return;;
+		'28') echo '' && return;;
+		'29') echo '' && return;;
+		'30') echo '' && return;;
+		'31') echo '' && return;;
+		*) echo "ERROR: invalid day entry: $1" && exit 1;
+	esac
+}
+
+function get_feb_prolouge() {
+	case $1 in
+		'01') echo '' && return;;
+		'02') echo '' && return;;
+		'03') echo '' && return;;
+		'04') echo '' && return;;
+		'05') echo '' && return;;
+		'06') echo '' && return;;
+		'07') echo '' && return;;
+		'08') echo '' && return;;
+		'09') echo '' && return;;
+		'10') echo '' && return;;
+		'11') echo '' && return;;
+		'12') echo '' && return;;
+		'13') echo '' && return;;
+		'14') echo '' && return;;
+		'15') echo '' && return;;
+		'16') echo '' && return;;
+		'17') echo '' && return;;
+		'18') echo '' && return;;
+		'19') echo '' && return;;
+		'20') echo '' && return;;
+		'21') echo '' && return;;
+		'22') echo '' && return;;
+		'23') echo '' && return;;
+		'24') echo '' && return;;
+		'25') echo '' && return;;
+		'26') echo '' && return;;
+		'27') echo '' && return;;
+		'28') echo '' && return;;
+		'29') echo '' && return;;
+		'30') echo '' && return;;
+		'31') echo '' && return;;
+		*) echo "ERROR: invalid day entry: $1" && exit 1;
+	esac
+}
+
+function get_mar_prolouge() {
+	case $1 in
+		'01') echo '' && return;;
+		'02') echo '' && return;;
+		'03') echo '' && return;;
+		'04') echo '' && return;;
+		'05') echo '' && return;;
+		'06') echo '' && return;;
+		'07') echo '' && return;;
+		'08') echo '' && return;;
+		'09') echo '' && return;;
+		'10') echo '' && return;;
+		'11') echo '' && return;;
+		'12') echo '' && return;;
+		'13') echo '' && return;;
+		'14') echo '' && return;;
+		'15') echo '' && return;;
+		'16') echo '' && return;;
+		'17') echo '' && return;;
+		'18') echo '' && return;;
+		'19') echo '' && return;;
+		'20') echo '' && return;;
+		'21') echo '' && return;;
+		'22') echo '' && return;;
+		'23') echo '' && return;;
+		'24') echo '' && return;;
+		'25') echo '' && return;;
+		'26') echo '' && return;;
+		'27') echo '' && return;;
+		'28') echo '' && return;;
+		'29') echo '' && return;;
+		'30') echo '' && return;;
+		'31') echo '' && return;;
+		*) echo "ERROR: invalid day entry: $1" && exit 1;
+	esac
+}
+
+function get_apr_prolouge() {
+	case $1 in
+		'01') echo '' && return;;
+		'02') echo '' && return;;
+		'03') echo '' && return;;
+		'04') echo '' && return;;
+		'05') echo '' && return;;
+		'06') echo '' && return;;
+		'07') echo '' && return;;
+		'08') echo '' && return;;
+		'09') echo '' && return;;
+		'10') echo '' && return;;
+		'11') echo '' && return;;
+		'12') echo '' && return;;
+		'13') echo '' && return;;
+		'14') echo '' && return;;
+		'15') echo '' && return;;
+		'16') echo '' && return;;
+		'17') echo '' && return;;
+		'18') echo '' && return;;
+		'19') echo '' && return;;
+		'20') echo '' && return;;
+		'21') echo '' && return;;
+		'22') echo '' && return;;
+		'23') echo '' && return;;
+		'24') echo '' && return;;
+		'25') echo '' && return;;
+		'26') echo '' && return;;
+		'27') echo '' && return;;
+		'28') echo '' && return;;
+		'29') echo '' && return;;
+		'30') echo '' && return;;
+		'31') echo '' && return;;
+		*) echo "ERROR: invalid day entry: $1" && exit 1;
+	esac
+}
+
+function get_may_prolouge() {
+	case $1 in
+		'01') echo '' && return;;
+		'02') echo '' && return;;
+		'03') echo '' && return;;
+		'04') echo '' && return;;
+		'05') echo '' && return;;
+		'06') echo '' && return;;
+		'07') echo '' && return;;
+		'08') echo '' && return;;
+		'09') echo '' && return;;
+		'10') echo '' && return;;
+		'11') echo '' && return;;
+		'12') echo '' && return;;
+		'13') echo '' && return;;
+		'14') echo '' && return;;
+		'15') echo '' && return;;
+		'16') echo '' && return;;
+		'17') echo '' && return;;
+		'18') echo '' && return;;
+		'19') echo '' && return;;
+		'20') echo '' && return;;
+		'21') echo '' && return;;
+		'22') echo '' && return;;
+		'23') echo '' && return;;
+		'24') echo '' && return;;
+		'25') echo '' && return;;
+		'26') echo '' && return;;
+		'27') echo '' && return;;
+		'28') echo '' && return;;
+		'29') echo '' && return;;
+		'30') echo '' && return;;
+		'31') echo '' && return;;
+		*) echo "ERROR: invalid day entry: $1" && exit 1;
+	esac
+}
+
+function get_jun_prolouge() {
+	case $1 in
+		'01') echo '' && return;;
+		'02') echo '' && return;;
+		'03') echo '' && return;;
+		'04') echo '' && return;;
+		'05') echo '' && return;;
+		'06') echo '' && return;;
+		'07') echo '' && return;;
+		'08') echo '' && return;;
+		'09') echo '' && return;;
+		'10') echo '' && return;;
+		'11') echo '' && return;;
+		'12') echo '' && return;;
+		'13') echo '' && return;;
+		'14') echo '' && return;;
+		'15') echo '' && return;;
+		'16') echo '' && return;;
+		'17') echo '' && return;;
+		'18') echo '' && return;;
+		'19') echo '' && return;;
+		'20') echo '' && return;;
+		'21') echo '' && return;;
+		'22') echo '' && return;;
+		'23') echo '' && return;;
+		'24') echo '' && return;;
+		'25') echo '' && return;;
+		'26') echo '' && return;;
+		'27') echo '' && return;;
+		'28') echo '' && return;;
+		'29') echo '' && return;;
+		'30') echo '' && return;;
+		'31') echo '' && return;;
+		*) echo "ERROR: invalid day entry: $1" && exit 1;
+	esac
+}
+
+function get_jul_prolouge() {
+	case $1 in
+		'01') echo '' && return;;
+		'02') echo '' && return;;
+		'03') echo '' && return;;
+		'04') echo '' && return;;
+		'05') echo '' && return;;
+		'06') echo '' && return;;
+		'07') echo '' && return;;
+		'08') echo '' && return;;
+		'09') echo '' && return;;
+		'10') echo '' && return;;
+		'11') echo '' && return;;
+		'12') echo '' && return;;
+		'13') echo '' && return;;
+		'14') echo '' && return;;
+		'15') echo '' && return;;
+		'16') echo '' && return;;
+		'17') echo '' && return;;
+		'18') echo '' && return;;
+		'19') echo '' && return;;
+		'20') echo '' && return;;
+		'21') echo '' && return;;
+		'22') echo '' && return;;
+		'23') echo '' && return;;
+		'24') echo '' && return;;
+		'25') echo '' && return;;
+		'26') echo '' && return;;
+		'27') echo '' && return;;
+		'28') echo '' && return;;
+		'29') echo '' && return;;
+		'30') echo '' && return;;
+		'31') echo '' && return;;
+		*) echo "ERROR: invalid day entry: $1" && exit 1;
+	esac
+}
+
+function get_aug_prolouge() {
+	case $1 in
+		'01') echo '' && return;;
+		'02') echo '' && return;;
+		'03') echo 'https://youtu.be/EJoZ8gxfMis' && return;;
+		'04') echo '' && return;;
+		'05') echo '' && return;;
+		'06') echo '' && return;;
+		'07') echo '' && return;;
+		'08') echo '' && return;;
+		'09') echo '' && return;;
+		'10') echo '' && return;;
+		'11') echo '' && return;;
+		'12') echo '' && return;;
+		'13') echo '' && return;;
+		'14') echo '' && return;;
+		'15') echo '' && return;;
+		'16') echo 'https://youtu.be/sxbV6MaLzsE' && return;;
+		'17') echo '' && return;;
+		'18') echo '' && return;;
+		'19') echo '' && return;;
+		'20') echo '' && return;;
+		'21') echo '' && return;;
+		'22') echo '' && return;;
+		'23') echo '' && return;;
+		'24') echo '' && return;;
+		'25') echo '' && return;;
+		'26') echo '' && return;;
+		'27') echo '' && return;;
+		'28') echo '' && return;;
+		'29') echo '' && return;;
+		'30') echo '' && return;;
+		'31') echo '' && return;;
+		*) echo "ERROR: invalid day entry: $1" && exit 1;
+	esac
+}
+
+function get_prologue() {
+	case $1 in
+		'Sep') get_sep_prolouge "$2";;
+		'Oct') get_oct_prolouge "$2";;
+		'Nov') get_nov_prolouge "$2";;
+		'Dec') get_dec_prolouge "$2";;
+		'Jan') get_jan_prolouge "$2";;
+		'Feb') get_feb_prolouge "$2";;
+		'Mar') get_mar_prolouge "$2";;
+		'Apr') get_apr_prolouge "$2";;
+		'May') get_may_prolouge "$2";;
+		'Jun') get_jun_prolouge "$2";;
+		'Jul') get_jul_prolouge "$2";;
+		'Aug') get_aug_prolouge "$2";;
+		*) echo "ERROR: invalid month entry: $1" && exit 1
+	esac
+}
+
+function get_daily_prolouge() {
+	local MONTH=$(date -d @$1 +%b)
+	local DAY=$(date -d @$1 +%d)
+	echo "$(get_prologue $MONTH $DAY)"
+	
+}
+
+CAL_DIFF=1123200
+CURRENT_NOW=$(date +%s)
+NEW_CAL_NOW=$(($CURRENT_NOW - 0))
+OLD_CAL_NOW=$(($CURRENT_NOW - $CAL_DIFF))
+
+case $1 in
+	'new') 	echo "$(get_daily_prolouge $NEW_CAL_NOW)" && exit;;
+	'old') 	echo "$(get_daily_prolouge $OLD_CAL_NOW)" && exit;;
+	'') 	echo 'please supply argument for new or old calendar. [new/old]'
+		echo './script new	or	./script old' && exit 1 ;;
+	*) 	echo 'Valid options are "new" or "old"!' && exit 1 ;;
+esac
+
+
+
+
+
+
+
+
